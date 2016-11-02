11f25763eec225a8fa9b9623f12044f120340645
spinningCat <coderberat@gmail.com>
2016-11-02 20:22:35 +0200

    HTTP, HTTPS, FTP, FTPS request with reflection with frontend support

---
+++ requestresponsereflection/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Runtime.Remoting;
+using System.Reflection;
+
+namespace requestresponsereflection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string protocol = "http://";
+            var assembly = Assembly.GetAssembly(typeof(WebRequest));
+            var objInstance = Activator.CreateInstance(assembly.FullName, "System.Net.HttpWebRequest").Unwrap();
+            objInstance = WebRequest.Create(protocol + "www.contoso.com/");
+          ;
+
+        }
+    }
+}
